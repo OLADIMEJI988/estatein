6314cf9be3a96ee36aab8dad1a9b16226779bbff
OLADIMEJI988 <oladimejimax49@gmail.com>
2025-11-03 05:54:17 +0100

    fixed light mode issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: 'media',
   theme: {
     screens: {
         mobile: "300px",
